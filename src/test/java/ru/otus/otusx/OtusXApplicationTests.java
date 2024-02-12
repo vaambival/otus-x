398c42d26e78596614adf457d884ee0d75e98d94
Vladimir.Kurov <vaambival@gmail.com>
2024-02-12 11:39:51 +0300

    Initial commit

---
+++ src/test/java/ru/otus/otusx/OtusXApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.otusx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
