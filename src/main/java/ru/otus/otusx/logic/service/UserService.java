11ff952e4084d6b41ce74b83db275c93419ae100
Vladimir.Kurov <vaambival@gmail.com>
2024-02-22 14:32:21 +0300

    hometask-02: Расширен API методом поиска по префиксу имени и префиксу фамилии

--- src/main/java/ru/otus/otusx/logic/service/UserService.java
+++ src/main/java/ru/otus/otusx/logic/service/UserService.java
@@ -3,6 +3,7 @@ package ru.otus.otusx.logic.service;
 import ru.otus.otusx.logic.dto.UserDto;
 import ru.otus.otusx.logic.dto.request.UserRegistrationRequest;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserService {
@@ -10,4 +11,6 @@ public interface UserService {
     UserDto getUser(UUID uuid);
 
     UUID register(UserRegistrationRequest request);
+
+    List<UserDto> search(String name, String surname);
 }
--- src/main/java/ru/otus/otusx/web/UserController.java
+++ src/main/java/ru/otus/otusx/web/UserController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.otus.otusx.logic.dto.request.UserRegistrationRequest;
 import ru.otus.otusx.logic.service.UserService;
 import ru.otus.otusx.logic.dto.UserDto;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -29,5 +31,10 @@ public class UserController {
     @PostMapping("/register")
     public UUID register(@RequestBody @Valid UserRegistrationRequest request) {
         return userService.register(request);
+    }
+
+    @GetMapping("/search")
+    public List<UserDto> search(@RequestParam("first_name") String name, @RequestParam("last_name") String surname) {
+        return userService.search(name, surname);
     }
 }
